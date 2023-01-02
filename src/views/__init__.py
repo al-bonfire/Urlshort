a66f089725ce28518cf633c5651da9c98b007f97
Antonio Fernando <antonio.fernando@mail.com>
2023-01-01 22:16:43 -0300

    atualizando

--- src/views/__init__.py
+++ src/views/__init__.py
@@ -2,4 +2,4 @@ from src.views import router
 from flask import Flask
 
 def init_app(app: Flask):
-    app.register_blueprint(router.bp, url_prefix='/encurtador')
+    app.register_blueprint(router.bp, url_prefix='/')
